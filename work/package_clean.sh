8be6b5b636cf3a23ac7b52e258c83ee91105808e
Franklin Ronald <franklin@wiselabs.com.br>
2016-09-03 14:01:38 -0300

    diretorio de trabalho removido

--- work/package_clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm *.gem *.tar.bz2 2> /dev/null
-rm -rf doc 2> /dev/null
-rm -f zipruby-*.tar.bz 2> /dev/null
-rm -f zipruby*.gem 2> /dev/null
-rm -f ext/zipruby/*.[ch] 2> /dev/null
-rm -rf work 2> /dev/null
