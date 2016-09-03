5e7808622a54d12d5374b6f040813af07ebaf730
Franklin Ronald <franklin@wiselabs.com.br>
2016-09-03 14:01:07 -0300

    Correcao no build

---
+++ work/package_clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm *.gem *.tar.bz2 2> /dev/null
+rm -rf doc 2> /dev/null
+rm -f zipruby-*.tar.bz 2> /dev/null
+rm -f zipruby*.gem 2> /dev/null
+rm -f ext/zipruby/*.[ch] 2> /dev/null
+rm -rf work 2> /dev/null
